173d11c1aeb36ffb5eac0480095f0ae1581d8df6
Marilyn <140780754+DevCodeJMMarilyn@users.noreply.github.com>
2023-08-11 11:58:55 -0600

    Practica 1 Grupo 1
    Programación estructurada

---
+++ Practica 1/js/index.js
@@ -0,0 +1,36 @@
+// let edad = prompt("Introduzca su edad: ")
+
+// if (edad > 18 && edad <= 100){
+//     alert ("El usuario es mayor de edad")
+
+// }else if(edad < 18 && edad > 1){
+//     alert ("El usuario es menor de edad")
+
+// }else if(edad == 18){
+//     alert ("El usuario tiene 18 años")
+    
+// }else if(edad > 100){
+//     alert ("El usuario es centenario")
+    
+// }else if(edad <= 0){
+//     alert ("Edad invalida")
+// }
+
+// //Sumar numeros ciclo while
+// let i = 1;
+// let suma = 0;
+
+// while(i <= 100){
+//     suma += i ;
+//     i++;
+// }
+// alert (suma);
+
+//calculadora 
+let num = prompt("Escriba un numero: ");
+
+for( i = 1; i <= 10; i++){
+    alert (num + " por " + i + " es igual a : ", (num*i));
+    //alert ( `${num} x ${i} = ${num * i}`)
+}
+
